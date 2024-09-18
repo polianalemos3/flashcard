07bcd286704c2c856491c3e0ac84780ba5a031be
Poliana Lemos <110950660+polianalemos3@users.noreply.github.com>
2024-09-18 19:32:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Conhecimento geral',
+    'Quem foi Getúlio Vargas?',
+    'Foi o Presidente do Brasil em 1951'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 6*6 ',
+    '36'
+)
+
+criaCartao(
+    'Lingua Espanhola',
+    'Como se fala oi tudo bem?',
+    'Ola, que tal?'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz feliz em Inglês?',
+    'Happy'
+)
+
+criaCartao(
+    'Se divertiu?',
+    'Não',
+    'SIM'
+)
