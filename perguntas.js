e6ec1d1f9961c40db057a7b643fab53b15f6cc58
Poliana Lemos <110950660+polianalemos3@users.noreply.github.com>
2024-09-25 19:35:29 -0300

    fl

--- perguntas.js
+++ perguntas.js
@@ -27,3 +27,37 @@ criaCartao(
     'Não',
     'SIM'
 )
+
+criaCartao(
+    'Conhecimento geral',
+    'De quem é a famosa frase “Penso, logo existo”?´
+c) Descartes
+)
+
+criaCartao(
+    'conhecimento geral',
+    ' De onde é a invenção do chuveiro elétrico?
+
+c) Brasil
+)
+
+criaCartao(
+    'Quais o menor e o maior país do mundo?
+a) Vaticano e Rússia
+)
+
+criaCartao(
+    'conhecimento geral',
+    ' Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Goulart'
+)
+
+criaCartao(
+    'SQual o número mínimo de jogadores em cada time numa partida de futebol?'
+    ' 7.'
+)
+
+criaCartao(
+    ' Quais as duas datas que são comemoradas em novembro?',
+    'Proclamação da República e Dia Nacional da Consciência Negra'
+)
