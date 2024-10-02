ea0f6860413140a49594542ef733c7fe9554ed0a
Poliana Lemos <110950660+polianalemos3@users.noreply.github.com>
2024-10-02 19:15:31 -0300

    poli

--- perguntas.js
+++ perguntas.js
@@ -3,49 +3,59 @@ criaCartao(
     'Quem foi Getúlio Vargas?',
     'Foi o Presidente do Brasil em 1951'
 )
+
 CriaCartao(
     'Matemática',
     'Quanto é 6*6 ',
     '36'
 )
+
 CriaCartao(
     'Lingua Espanhola',
     'Como se fala oi tudo bem?',
     'Ola, que tal?'
 )
+
 criaCartao(
     'Lingua inglesa',
     'Como se diz feliz em Inglês?',
     'Happy'
 )
+
 criaCartao(
     'Se divertiu?',
     'Não',
     'SIM'
 )
+
 criaCartao(
     'Conhecimento geral',
-    'De quem é a famosa frase “Penso, logo existo”?´
+    'De quem é a famosa frase “Penso, logo existo”?',
      'Descartes'
 )
+
 criaCartao(
     'conhecimento geral',
-    ' De onde é a invenção do chuveiro elétrico
+    ' De onde é a invenção do chuveiro elétrico',
      'Brasil'
 )
 criaCartao(
-    'Quais o menor e o maior país do mundo?
+    'Geografia',
+    'Quais o menor e o maior país do mundo?',
     'Vaticano e Rússia'
 )
+
 criaCartao(
     'conhecimento geral',
     ' Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
     'João Goulart'
 )
+
 criaCartao(
-    'SQual o número mínimo de jogadores em cada time numa partida de futebol?'
+    'SQual o número mínimo de jogadores em cada time numa partida de futebol?',
     ' 7.'
 )
+
 criaCartao(
     ' Quais as duas datas que são comemoradas em novembro?',
     'Proclamação da República e Dia Nacional da Consciência Negra'
